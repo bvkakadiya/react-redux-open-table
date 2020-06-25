39207de770a771057288ea024e66aa232c66528d
Bhavesh Kakadiya <bhaveshkakadiya24@gmail.com>
2020-06-25 16:40:19 -0400

    heroku deploy

---
+++ build/precache-manifest.1dc897bf0ac191313648e7af3f635842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe4f29e99e3b94ecadc6e093dbad847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07fd0c09e0d16405c07b",
+    "url": "/static/css/main.63adb012.chunk.css"
+  },
+  {
+    "revision": "fb56f6eea58ec074fb3b",
+    "url": "/static/js/2.6cb147ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6cb147ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07fd0c09e0d16405c07b",
+    "url": "/static/js/main.d305c17e.chunk.js"
+  },
+  {
+    "revision": "1847a1ba43c6a13b15c1",
+    "url": "/static/js/runtime-main.06884c82.js"
+  }
+]);
